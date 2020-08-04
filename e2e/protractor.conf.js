6c24aea33e0e8523d0cfe65854fa067a9eeb60e5
meera <meerassiet@gmail.com>
2020-08-04 22:06:16 +0530

    Updates ipaddress

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
         browserName: 'chrome'
     },
     directConnect: true,
-    baseUrl: 'http://localhost:4200/',
+    baseUrl: 'http://18.223.114.221:4200/',
     framework: 'jasmine',
     jasmineNodeOpts: {
         showColors: true,
